19e98dbf0f0cea94728c54532fac837c6722c071
Ryan Shaw <shawryc@gmail.com>
2015-09-16 09:55:08 -0700

    Add a unit testing!

--- learning_python.py
+++ learning_python.py
@@ -1,8 +1,21 @@
 def fibonacci():
     """Returns a generator of fibonacci serires."""
-    a, b = 1, 1
+    a, b = 0, 1
     while True:
-        yield b
         a, b = b, a + b
+        yield a
 
+import types
+def test_fibonacci():
+    assert(type(fibonacci()) == types.GeneratorType)
+    print("Good, this fibonacci() returns a generator.")
+    expected = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+    counter = 0
+    for n in fibonacci():
+        print(n)
+        assert(n == expected[counter])
+        counter += 1
+        if (counter >= 10):
+            break
 
+test_fibonacci()
